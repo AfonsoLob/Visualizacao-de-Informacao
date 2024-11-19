2f026d4261ea19ac9324ee05f05390ad57a347f6
Alob <afonso.baixo@ua.pt>
2024-11-19 20:23:32 +0000

    adição do violin plot

--- UA_Insights/src/pages/Cadeira.jsx
+++ UA_Insights/src/pages/Cadeira.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useRef } from "react";
 import CourseApprovalsByExamType from "../components/CourseApprovalsByExamType";
 import ApprovedPercentagePerYear from "../components/CourseApprovalPercentByYear";
+import AverageGradeRadar from "../components/CourseGradeRadar";
+import GradeViolinPlot from "../components/GradeViolinPlot";
 
 const Cadeira = () => {
   
@@ -13,10 +15,11 @@ const Cadeira = () => {
           <CourseApprovalsByExamType/>
         </div>
         <div className="w-full h-[400px] border border-lightgray rounded-lg p-1.5 bg-[#f9f9f9] flex flex-col items-center justify-center text-white" style={{ backgroundColor: '#2d3748' }}>
-            <h2>Nota Final / Cadeira</h2>
+            <GradeViolinPlot/>
         </div>
         <div className="w-full h-[400px] border border-lightgray rounded-lg p-1.5 bg-[#f9f9f9] flex flex-col items-center justify-center text-white" style={{ backgroundColor: '#2d3748' }}>
-          <h2>Nota Final / Cursos</h2>
+          <h2>Something</h2>
+          {/* <AverageGradeRadar/> */}
         </div>
       </div>
     );
