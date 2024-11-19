443c35bf8678bb69ba56eea76ed5220dcc76eeb9
Alob <afonso.baixo@ua.pt>
2024-11-19 17:21:16 +0000

    router to /cadeira

--- UA_Insights/src/utils/Router.jsx
+++ UA_Insights/src/utils/Router.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Base from '../layout/Base';
 import Universidade from '../pages/Universidade';
+import Cadeira from '../pages/Cadeira';
 import Dashboard from '../pages/Dashboard';
 //import GraphicScripts from '../components/GraphicScripts'; // Adjust the path as needed
 
@@ -12,6 +13,7 @@ const AppRouter = () => (
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/universidade" element={<Universidade />} />
+        <Route path="/cadeira" element={<Cadeira />} />
         {/* <Route path="/graphics" element={<GraphicScripts />} /> */}
         {/* Add other routes here */}
       </Routes>
