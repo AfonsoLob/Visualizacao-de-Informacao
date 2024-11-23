fc9a7e15eebc79914174cf4edc13bc0f7ffd6d69
Alob <afonso.baixo@ua.pt>
2024-11-23 19:37:43 +0000

    a bit of responsivenessC

--- UA_Insights/src/layout/Navbar.jsx
+++ UA_Insights/src/layout/Navbar.jsx
@@ -1,74 +1,102 @@
-import { Link , useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { useState } from 'react';
 
 export const Navbar = () => {
+  const location = useLocation();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    const location = useLocation();
+  const isActive = (path) => {
+    return location.pathname === path ? 'btn-active bg-primary' : '';
+  };
 
-    const isActive = (path) => {
-        return location.pathname === path ? "btn-active bg-primary" : "";
-    };
-    
-    return (
-        <div className="navbar bg-gray-800 rounded-xl">
-            <div className="navbar-start">
-                { isActive("/") ?
-                <Link to="/" className="btn btn-primary text-xl text-white">
-                    UA Insights
-                </Link>
-                :
-                <Link to="/" className="btn btn-ghost text-xl text-white">
-                    UA Insights
-                </Link>
-                }
-            </div>
-            
-            <div className="navbar-center">
-                <div className="flex gap-4">
+  return (
+    <div className="navbar bg-gray-800 rounded-xl flex justify-between">
+      {/* Navbar Start */}
+      <div className="navbar-start">
+        <Link to="/" className={`btn text-xl text-white ${isActive("/") ? "btn-primary" : "btn-ghost"}`}>
+          UA Insights
+        </Link>
+      </div>
 
-                    { isActive("/universidade") ?
-                    <Link to="/universidade" className="btn btn-primary text-white">
-                        Universidade
-                    </Link>
-                    :
-                    <Link to="/universidade" className="btn btn-ghost text-white ">
-                        Universidade
-                    </Link>
-                    }
-
-                    { isActive("/departamento") ?
-                    <Link to="/departamento" className="btn btn-primary text-white">
-                        Departamento
-                    </Link>
-                    :
-                    <Link to="/departamento" className="btn btn-ghost text-white">
-                        Departamento
-                    </Link>
-                    }
-
-                    { isActive("/curso") ?
-                    <Link to="/curso" className="btn btn-primary text-white">
-                        Curso
-                    </Link>
-                    :
-                    <Link to="/curso" className="btn btn-ghost text-white">
-                        Curso
-                    </Link>
-                    }
-
-                    { isActive("/cadeira") ?
-                    <Link to="/cadeira" className="btn btn-primary text-white">
-                        Cadeira
-                    </Link>
-                    :
-                    <Link to="/cadeira" className="btn btn-ghost text-white">
-                        Cadeira
-                    </Link>
-                    }              
-                </div>
-            </div>
-            
-            <div className="navbar-end">
-            </div>
+      {/* Navbar Center */}
+      <div className="navbar-center hidden lg:flex">
+        <div className="flex gap-4">
+          <Link to="/universidade" className={`btn text-white ${isActive("/universidade") ? "btn-primary" : "btn-ghost"}`}>
+            Universidade
+          </Link>
+          <Link to="/departamento" className={`btn text-white ${isActive("/departamento") ? "btn-primary" : "btn-ghost"}`}>
+            Departamento
+          </Link>
+          <Link to="/curso" className={`btn text-white ${isActive("/curso") ? "btn-primary" : "btn-ghost"}`}>
+            Curso
+          </Link>
+          <Link to="/cadeira" className={`btn text-white ${isActive("/cadeira") ? "btn-primary" : "btn-ghost"}`}>
+            Cadeira
+          </Link>
         </div>
-    );
-}
+      </div>
+
+     
+      {/* Dropdown Menu for Smaller Screens */}
+      <div className="dropdown dropdown-end lg:hidden">
+        <label tabIndex={0} className="btn btn-ghost text-white lg:hidden">
+            <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+            >
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 5.25h16.5m-16.5 6h16.5m-16.5 6h16.5"
+            />
+            </svg>
+        </label>
+        <ul
+            tabIndex={0}
+            className="z-10 dropdown-content menu p-2 shadow bg-gray-800 rounded-box w-48 mt-2"
+        >
+            <li>
+            <Link
+                to="/universidade"
+                className={`text-white ${isActive("/universidade") ? "btn-primary" : "btn-ghost"}`}
+            >
+                Universidade
+            </Link>
+            </li>
+            <li>
+            <Link
+                to="/departamento"
+                className={`text-white ${isActive("/departamento") ? "btn-primary" : "btn-ghost"}`}
+            >
+                Departamento
+            </Link>
+            </li>
+            <li>
+            <Link
+                to="/curso"
+                className={`text-white ${isActive("/curso") ? "btn-primary" : "btn-ghost"}`}
+            >
+                Curso
+            </Link>
+            </li>
+            <li>
+            <Link
+                to="/cadeira"
+                className={`text-white ${isActive("/cadeira") ? "btn-primary" : "btn-ghost"}`}
+            >
+                Cadeira
+            </Link>
+            </li>
+        </ul>
+        </div>
+
+      <div className='navbar-end sm:hidden lg:block'>
+
+      </div>
+    </div>
+  );
+};
