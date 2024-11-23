bcaeeaef5512fc63281949e9cba59b50dfc36e4e
Alob <afonso.baixo@ua.pt>
2024-11-23 22:04:34 +0000

    margin fix

--- UA_Insights/src/components/cadeira/ApprovalPercentage.jsx
+++ UA_Insights/src/components/cadeira/ApprovalPercentage.jsx
@@ -60,7 +60,7 @@ return (
           <ResponsiveContainer width="100%" height={300}>
             <BarChart
               data={data}
-              margin={{ top: 10, right: 30, left: 10, bottom: 0 }}
+              margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
             >
               <CartesianGrid strokeDasharray="3 3" />
               <XAxis dataKey="year" stroke="#ffffff" />
