b9e50b5b023b8ef9dc11c999070deb655e012405
Alob <afonso.baixo@ua.pt>
2024-11-24 18:10:50 +0000

    context for rawData2

--- UA_Insights/src/layout/Base.jsx
+++ UA_Insights/src/layout/Base.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import { Filter } from './Filter';
 import { Navbar } from './Navbar';
+import { DataProvider } from '../context/DataContext';
 import { FilterProvider } from '../context/FilterContext';
 import { CourseProvider } from '../context/courseContext';
 const Base = ({ children }) => {
   return (
-    <FilterProvider>
-      <CourseProvider>
-        <div className="flex min-h-screen">
-          <Filter />
-          <div className="flex-1 p-[0.8rem] flex flex-col">
-            <Navbar />
-            {children}
+    <DataProvider>
+      <FilterProvider>
+        <CourseProvider>
+          <div className="flex min-h-screen">
+            <Filter />
+            <div className="flex-1 p-[0.8rem] flex flex-col">
+              <Navbar />
+              {children}
+            </div>
           </div>
-        </div>
-      </CourseProvider>
-    </FilterProvider>
+        </CourseProvider>
+      </FilterProvider>
+    </DataProvider>
   );
 };
 
