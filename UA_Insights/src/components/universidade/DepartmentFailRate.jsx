335dbc0931dce4ad69770c2251db5601c812ecda
Alob <afonso.baixo@ua.pt>
2024-11-25 15:20:18 +0000

    color small fix

--- UA_Insights/src/components/universidade/DepartmentFailRate.jsx
+++ UA_Insights/src/components/universidade/DepartmentFailRate.jsx
@@ -58,7 +58,7 @@ const DepartmentFailRate = () => {
   // Generate colors for departments
   const colors = [
     "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", 
-    "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf", 
+    "#8c564b", "#e377c2", "#ff7f7f", "#bcbd22", "#17becf", 
     "#f7b6d2", "#c7c7c7", "#9edae5", "#ffbb78", "#98df8a", 
     "#ff9896", "#c5b0d5", "#c49c94"
   ];
